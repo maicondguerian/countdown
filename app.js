808db725b988347946ba6355ea048069f9bf3fd1
maicondguerian <maicondguerian@gmail.com>
2022-12-08 18:14:39 -0300

    adding js countdown function

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const countDown = () =>{
     const minute = second * 60;
     const hour = minute *60;
     const day = hour * 24;
+
+    // calculando
+
+    const remainDay = Math.floor(gape / day)
+    const remainHour = Math.floor((gape % day)/ hour)
+    const remainMinute = Math.floor((gape % hour)/ minute)
+    const remainSecond = Math.floor((gape % minute)/ second)
+
+    document.querySelector('.day').innerHTML= remainDay
+    document.querySelector('.hour').innerHTML= remainHour
+    document.querySelector('.minute').innerHTML= remainMinute
+    document.querySelector('.second').innerHTML= remainSecond
+    
 }
 
-countDown();
+setInterval(countDown,1000)
